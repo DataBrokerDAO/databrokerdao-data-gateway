b2814156fa17e366703e9f8160ba5c10b1785743
Main <nvde1960@gmail.com>
2019-02-15 14:48:10 +0100

    It works atm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import * as express from 'express';
 import * as mongoDbConnection from './services/database/mongoDbConnection'
+import * as enlistSensors from './scripts/enlistSensors';
+import {ISensor} from './services/model/sensor';
 
 require('dotenv').load();
 
@@ -8,10 +10,28 @@ globalAny.databaseIsInititialized = false;
 globalAny.sensorData = [];
 
 const app = express();
+//TODO:  Fix process.env
 const port = process.env.MIDDLEWARE_PORT || 3000; 
 const intervalTimeInSeconds = 5;
 
-console.log(process.env);
+var tempSensor: ISensor = {
+  price: '50',
+  stakeamount: '400',
+  metadata: {
+    name: 'Test',
+    sensorid: '345',
+    geo: {
+      lat: 12.145,
+      lng: 19.93
+    },
+    type: 'UM12',
+    example: 'example',
+    updateinterval: 500,
+    sensortype: 'UM14'
+  }
+}
+
+//console.log(process.env);
 
 function bootstrap() {
   setTimeout(bootstrap, 1000 * intervalTimeInSeconds);
@@ -26,4 +46,6 @@ function bootstrap() {
   // });
 }
 console.log('Hi!');
+console.log('Running sensor enlisting script');
+enlistSensors.enlistSensor(tempSensor);
 bootstrap();
